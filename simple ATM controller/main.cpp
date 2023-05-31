7eed4b96f345835799e6a2d3cdf9ba07439876e1
onsoim <onsoim@gmail.com>
2023-05-31 13:25:10 +0900

    Initial Commit

---
+++ simple ATM controller/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  simple ATM controller
+//
+//  Created by onsoim on 2023/05/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
